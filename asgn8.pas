59e7e0d4344ca93c91869e3969af42ad8aae9c4f
jkalsi1 <jkalsi@calpoly.edu>
2024-06-06 11:25:02 -0700

    parse working for appC

--- asgn8.pas
+++ asgn8.pas
@@ -1,4 +1,4 @@
-program TestCasesExample;
+program asgn8;
 
 {$MODE OBJFPC} // directive to be used for creating classes
 {$M+} // directive that allows class constructors and destructors
@@ -340,26 +340,45 @@ end;
 var
   keywords: array[0..8] of string = ('''true', '''false', '''lamb', '''locals',
   '''if', ''':', '''{', '''}', ''':=');
+  primOps: array of string = ('+', '-', '*', '/', '<=');
+
+function IsPrimOp(const op: string) : Boolean;
+var
+  i: Integer;
+begin
+  Result := false; // Initialize Result to false
+  for i := Low(primOps) to High(primOps) do
+  begin
+    if primOps[i] = op then
+    begin
+      Result := true; // Set Result to true if operator is found
+      Exit;
+    end;
+  end;
+end;
 
 function IsKeyword(const S: string) : Boolean;
 var
   i: Integer;
 begin
+  Result := false; // Initialize Result to false
   for i := Low(keywords) to High(keywords) do
   begin
     if keywords[i] = S then
-       begin
-         Result := false;
-         Exit;
-       end;
+    begin
+      Result := true; // Set Result to true if keyword is found
+      Exit;
+    end;
   end;
 end;
+
 
 // Check type
 function IsRealNumber(const S: string): Boolean;
 var
   R: Double;
 begin
+Result := false; // Initialize Result to false
   Result := TryStrToFloat(S, R);
 end;
 
@@ -370,17 +389,21 @@ end;
 
 function IsBool(const S: string) : Boolean;
 begin
+Result := false; // Initialize Result to false
   Result := (S = 'true') or (S = 'false');
 end;
 
 function IsSymbol(const S: string) : Boolean;
 begin
+Result := false; // Initialize Result to false
   Result := (Length(S) > 0) and (S[0] = '''') and IsKeyword(s);
 end;
 
 function parse(Sexp: array of string): ExprC;
 var
   s: string;
+  appCArgs: array of ExprC;
+  i: integer;
 begin
   if Length(Sexp) = 0 then
     raise Exception.Create('ZODE: parse: empty Sexp!')
@@ -388,26 +411,53 @@ begin
   begin
     s := Sexp[0];
     if IsRealNumber(s) then
-      Result := numV.Create(StrToFloat(s))
+    begin
+      Result := numV.Create(StrToFloat(s));
+      Exit;
+    end
     else if IsBool(s) then
     begin
       if s = 'true' then
         Result := boolV.Create(true)
       else
         Result := boolV.Create(false);
+      Exit;
+    end
+    else if IsRealString(s) then
+    begin
+      Result := stringC.Create(Copy(s, 2, Length(s) - 2));
+      Exit;
     end
     else if IsSymbol(s) then
-      Result := idC.Create(s)
-    else if IsRealString(s) then
-      Result := idC.Create(s)
+    begin
+      Result := idC.Create(s);
+      Exit;
+    end
+    else if (Length(Sexp) = 3) and IsPrimOp(Sexp[0]) then
+    begin
+      SetLength(appCArgs, 2);
+      appCArgs[0] := parse([Sexp[1]]);
+      appCArgs[1] := parse([Sexp[2]]);
+      Result := appC.Create(idC.Create(Sexp[0]), appCArgs);
+      Exit;
+    end
     else if (Length(Sexp) > 5) and (Sexp[0] = 'if') and (Sexp[1] = ':') and (Sexp[3] = ':') and (Sexp[5] = ':') then
-      Result := ifC.Create(parse([Sexp[2]]),
-                           parse([Sexp[4]]),
-                           parse([Sexp[6]]))
+    begin
+      Result := ifC.Create(parse([Sexp[2]]), parse([Sexp[4]]), parse([Sexp[6]]));
+      Exit;
+    end
     else
+    begin
+      writeln('unrecognized: ');
+      for i := 0 to High(Sexp) do
+      begin
+        writeln(Sexp[i]);
+      end;
       raise Exception.Create('ZODE: parse: unrecognized Sexp!');
+    end;
   end;
 end;
+
 
 
 // Test cases
@@ -433,17 +483,53 @@ end;
 
 procedure TestParse;
 var
-   numArr: array of string = ('1');
-   strArr: array of string = ('cow');
-   symArr: array of string = ('''cow');
-   boolTArr: array of string = ('false');
-   boolFArr: array of string = ('true');
-   keywordCheckArr: array of string = ('''true');
-   ifArr: array of string = ('if', ':', 'true', ':', '1', ':', '0');
+  numArr: array of string = ('1');
+  strArr: array of string = ('"cow"');
+  symArr: array of string = ('''cow');
+  boolTArr: array of string = ('true');
+  boolFArr: array of string = ('false');
+  keywordCheckArr: array of string = ('''true');
+  ifArr: array of string = ('if', ':', 'true', ':', '1', ':', '0');
+  primOpArr: array of string = ('+', '1', '2');
+  parsedExpr: ExprC;
+  expectedExpr: appC;
+  expectedArgs: argz;
 begin
-   assert(parse(numArr) = numC.Create(1), 'Test Parse failed');
-   writeln('Test Parse passed')
+  // Test parsing a number
+  writeln('testing parse on just numV');
+  assert(parse(numArr) is numV, 'Test Parse failed: numArr');
+
+  // Test parsing a string
+  writeln('testing parse on stringC');
+  assert(parse(strArr) is stringC, 'Test Parse failed: strArr');
+
+  // Test parsing a symbol
+  writeln('testing parse on idC');
+  assert(parse(symArr) is idC, 'Test Parse failed: symArr');
+
+  // Test parsing a boolean true
+  writeln('testing parse on boolV (true)');
+  assert(parse(boolTArr) is boolV, 'Test Parse failed: boolTArr');
+
+  // Test parsing a boolean false
+  writeln('testing parse on boolV (false)');
+  assert(parse(boolFArr) is boolV, 'Test Parse failed: boolFArr');
+
+  // Test parsing a primitive operation
+  writeln('testing parse on appC');
+  parsedExpr := parse(primOpArr);
+  assert(parsedExpr is appC, 'Test Parse failed: primOpArr is not appC');
+
+  // Create the expected appC object
+  SetLength(expectedArgs, 2);
+  expectedArgs[0] := numC.Create(1);
+  expectedArgs[1] := numC.Create(2);
+  expectedExpr := appC.Create(idC.Create('+'), expectedArgs);
+  assert (parsedExpr = expectedExpr, 'test parse failed: unexpected appC');
+  writeln('Test Parse passed');
 end;
+
+
 procedure TestPrimV;
 var
   p: primV;
